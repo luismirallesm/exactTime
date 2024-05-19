ca78c77005aaedb3028a00dd7952071d8a350e29
unknown <luismirallesm99@gmail.com>
2024-05-19 21:02:51 +0200

    added from the previous commit

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import './style.css'
 import Phaser from 'phaser'
+import { ScoreScene } from "./scoreScene.js";
+import { GameScene } from "./gameScene.js";
 
 const sizes = {
 	width: 500,
@@ -7,80 +9,27 @@ const sizes = {
 }
 
 
-class GameScene extends Phaser.Scene {
+class MainScene extends Phaser.Scene {
 	constructor() {
-		super("scene-game")
-		this.timedEvent;
-		this.btn;
-		this.guessTime;
-		this.timeText;
-		this.remainingTime;
-		this.countDown;
-		this.pressedTime;
-
+		super("MainScene");
 	}
-	preload() {
+		preload() {
 		this.load.image("bg", "/assets/background.jpg");
-		this.load.image("button", "/assets/redDot.png")
+		this.load.image("button", "/assets/redDot.png");
+		//
 	}
-
-	create() {
-		this.add.image(-70, 0, "bg").setOrigin(0, 0);
-		this.startButton = this.add.text(sizes.width / 2 - 60, 10, "Start", {
-			font: "50px Arial",
-			fill: "#ffffff"
-		});
-		this.countDown = Math.floor(Math.random() * 5) + 3;
-		this.btn = this.add.image(220, 425, "button").setScale(0.1).setOrigin(0, 0);
-		this.btn.setInteractive();
-		this.guessTime = Math.floor(Math.random() * 12) + 3;
-		this.timeText = this.add.text(sizes.width / 2-60, 110, this.guessTime + ":00", {
-			font: "50px Arial",
-			fill: "#ffffff"
-		});
-		this.timedEvent = this.time.delayedCall(6000 + this.guessTime * 1000, this.gameOver, [], this);
-		console.log(this.guessTime + 6);
-		console.log(this.timedEvent.getRemainingSeconds());
-		this.btn.on("pointerdown", () => {
-			this.pressedButton();
-		});
-		
-
-	}
-
-	update() {
-		this.remainingTime = Math.floor(this.timedEvent.getRemainingSeconds());
-		var startTime = Math.floor(this.guessTime + 6 - this.remainingTime);
-		
-		if (startTime < 4) {
-			this.timeText.setText(startTime);
-		} else {
-			if (this.remainingTime < this.countDown) {
-				this.timeText.setText(Math.round(this.remainingTime).toString());
-			} else {
-				this.timeText.setText(this.guessTime + ":00");
-			}
+	create(){
+		this.scene.start('GameScene')
 		}
 	}
-
-	gameOver() {
-		this.timeText.setText(this.pressedTime);
-	}
-
-	pressedButton() {
-		var factor = Math.pow(10, 3); // last number equals the quantity of decimals for the round
-		this.pressedTime = this.guessTime + 3 - this.timedEvent.getRemainingSeconds();
-		this.pressedTime = Math.round((this.pressedTime + Number.EPSILON) * factor) / factor;
-		console.log("pulsado en " + this.pressedTime);
-	}
-}
 
 const config = {
 	type: Phaser.WEBGL,
 	width: sizes.width,
 	height: sizes.height,
 	canvas: gameCanvas,
-	scene: [GameScene]
+	scene: [MainScene, GameScene, ScoreScene]
 }
+
 
 const game = new Phaser.Game(config)
